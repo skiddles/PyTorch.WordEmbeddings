188238ef94d7ad2e4c6d3805a0c9edaa562b6ed9
Skiddles <skiddles@users.noreply.github.com>
2018-07-09 16:55:06 -0500

    Update PyTorch_WordEmbedding_BeigeBook.py

--- PyTorch_WordEmbedding_BeigeBook.py
+++ PyTorch_WordEmbedding_BeigeBook.py
@@ -7,7 +7,6 @@ import torch.optim as optim
 import numpy as np
 import time
 import argparse
-import copy
 import os
 
 
@@ -31,9 +30,9 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--clean', dest='clean_dir',  required=True, default=r'./TrigramDataset/trigrams_data/Corpus.pkl',
                         help='the directory to load the cleansed corpus from')
-    parser.add_argument('--clip', dest='clip', required=False,
+    parser.add_argument('--clip', dest='clip', required=False, default=None,
                         help='Limit the number of records for debugging purposes')
-    parser.add_argument('--save', dest='save_dir', required=True,default=r"./data/final_model.md",
+    parser.add_argument('--save', dest='save_dir', required=True, default=r"./data/final_model.md",
                         help='the directory to save and load the in-progress model from')
     parser.add_argument('--batch-size', dest='MINI_BATCH', type=int, default=1000, metavar='N',
                         help='input batch size for training (default: 1000)')
@@ -60,25 +59,35 @@ def save_checkpoint(state, filename):
     torch.save(state, filename)
 
 
+def train(model, optimizer):
+    optimizer.zero_grad()
+    # log_probs = model(torch.Tensor(text_data.context_ids[minibatchids]).long())
+    for data in dataloader:
+        log_probs = model(torch.LongTensor(data['context']))
+        y = torch.autograd.Variable(
+                torch.squeeze(torch.LongTensor(data['target'])))
+
+        loss = loss_function(log_probs, y)
+        loss.backward()
+        optimizer.step()
+    return loss
+
+
 if __name__ == '__main__':
+
+    loss_function = nn.NLLLoss()
+
     parms = get_args()
-
     torch.manual_seed(1)
-
     text_data = tgd.TrigramsDataset(parms)
-    if parms.clip:
-        text_data.prepare_trigrams(parms.clip)
-    else:
-        text_data.prepare_trigrams()
-
     dataloader = DataLoader(text_data,
                             batch_size=parms.MINI_BATCH,
-                            shuffle=True,
-                            num_workers=3)
+                            shuffle=False,
+                            num_workers=2)
 
-    model = NGramLanguageModeler(len(text_data.vocab), parms.EMBEDDING_DIM, parms.CONTEXT_SIZE)
-    model.share_memory()
-    # Define an optimizer
+    vocab_length = len(text_data.get_vocab())
+    model = NGramLanguageModeler(vocab_length, parms.EMBEDDING_DIM, parms.CONTEXT_SIZE)
+    # model.share_memory()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     if not os.path.exists(parms.save_dir):
@@ -90,17 +99,19 @@ if __name__ == '__main__':
     else:
         checkpoint = torch.load(parms.save_dir)
         parms.start_epoch = checkpoint['epoch']
-        print(model.embeddings)
         model.load_state_dict(checkpoint['state_dict'])
-        print(model.embeddings)
         optimizer.load_state_dict(checkpoint['optimizer'])
         print("=> loaded checkpoint '{}' (epoch {})"
               .format(parms.save_dir, checkpoint['epoch']))
+        f2 = open('./data/loaded_weights.txt', 'w')
+        for x in model.parameters():
+            f2.write(str(x.data))
+        f2.close()
+
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     losses = []
-    loss_function = nn.NLLLoss()
 
     if parms.start_epoch>0:
         print("start_epoch is %d" % parms.start_epoch)
@@ -110,70 +121,29 @@ if __name__ == '__main__':
         EPOCH_RANGE = range(0, parms.EPOCHS)
 
     for epoch in EPOCH_RANGE:
-
-        final_model_wts = copy.deepcopy(model.state_dict())
         current_start = 0
         total_loss = torch.Tensor([0])
         s_elapsed_time = 0
+        t = time.process_time()
 
-        keep_going = True
-        while keep_going:
-            if current_start + parms.MINI_BATCH < len(text_data.target_ids):
-                minibatchids = slice(current_start, current_start + parms.MINI_BATCH -1)
-            else:
-                minibatchids = slice(current_start, len(text_data.target_ids))
-                keep_going = False
+        loss = train(model, optimizer)
+        total_loss += loss.item()
+        elapsed_time = time.process_time() - t
 
-            t = time.process_time()
-            # Step 2. Recall that torch *accumulates* gradients. Before passing in a
-            # new instance, you need to zero out the gradients from the old
-            # instance
-            model.zero_grad()
-
-            # Step 3. Run the forward pass, getting log probabilities over next
-            # words
-            log_probs = model(torch.Tensor(text_data.context_ids[minibatchids]).long())
-
-            # Step 4. Compute your loss function. (Again, Torch wants the target
-            # word wrapped in a variable)
-            loss = loss_function(log_probs,
-                                 torch.autograd.Variable(
-                                     torch.squeeze(
-                                        torch.Tensor(text_data.target_ids[minibatchids]).long()
-                                        )
-                                     )
-                                 ) * parms.MINI_BATCH
-
-            # Step 5. Do the backward pass and update the gradient
-            loss.backward()
-
-            optimizer.step()
-
-            # Get the Python number from a 1-element Tensor by calling tensor.item()
-            total_loss += loss.item()
-            elapsed_time = time.process_time() - t
-            s_elapsed_time += elapsed_time
-            t_avg_time = elapsed_time/parms.MINI_BATCH
-            s_avg_time = s_elapsed_time/(current_start + parms.MINI_BATCH)
-            print('Epoch: %d  Batch Size: %s  Processed: %d  Batch Avg Time: %0.2f  EP Avg Time: %0.2f  '
-                  'Loss: %0.2f  TotalLoss: %0.1f' % (epoch, str.rjust(str(parms.MINI_BATCH), 6, ' '),
-                                                     current_start+parms.MINI_BATCH,
-                                                     np.log10(t_avg_time), np.log10(s_avg_time),
-                                                     loss.item(), total_loss.item()))
-            if keep_going:
-                current_start = current_start + parms.MINI_BATCH
+        print("Epoch: %d Elapsed Time %0.2f  Loss: %0.6f" % (epoch, np.log10(elapsed_time/float(parms.clip)), loss.item()))
         losses.append(total_loss)
-        # Check out this site https://discuss.pytorch.org/t/saving-and-loading-a-model-in-pytorch/2610/3
+
         save_checkpoint({
             'epoch': epoch + 1,
             'state_dict': model.state_dict(),
             'optimizer': optimizer.state_dict(),
         }, parms.save_dir)
 
+        f = open('./data/prev_weights.txt', 'w')
+        for x in model.parameters():
+            f.write(str(x.data))
+        f.close()
 
-# The loss decreased every iteration over the training data!
-# print(len(losses))
-# print(losses[-1].item())
 
 
 
